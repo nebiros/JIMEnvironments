e398d0dae3541c0b5c5c60f6514d39a3096cdd23
Juan Felipe Alvarez Saldarriaga <nebiros@gmail.com>
2015-06-22 18:26:34 -0500

    adds nullability.

--- JIMEnvironments/JIMEnvironments.m
+++ JIMEnvironments/JIMEnvironments.m
@@ -1,6 +1,6 @@
 //
 //  JIMEnvironments.m
-//  UneEmpresas
+//  juan.im
 //
 //  Created by Juan Felipe Alvarez Saldarriaga on 9/29/14.
 //  Copyright (c) 2014 juan.im. All rights reserved.
@@ -13,13 +13,14 @@
 NSString *const kJIMEnvironmentsEnvironmentsFilename = @"Environments";
 NSString *const kJIMEnvironmentsTargetBuildConfigurationName = @"JIMEnvironmentsTargetBuildConfigurationName";
 
-+ (instancetype)sharedInstance {
++ (instancetype)sharedInstance
+{
     static JIMEnvironments *_sharedInstance = nil;
     static dispatch_once_t onceToken;
     dispatch_once(&onceToken, ^{
         _sharedInstance = [[self alloc] init];
     });
-    
+
     return _sharedInstance;
 }
 
@@ -27,14 +28,14 @@ NSString *const kJIMEnvironmentsTargetBuildConfigurationName = @"JIMEnvironments
 {
     self = [super init];
     if (!self) return nil;
-    
+
     NSString *envsFile = [[NSBundle mainBundle] pathForResource:kJIMEnvironmentsEnvironmentsFilename ofType:@"plist"];
     NSAssert(envsFile && [envsFile length] > 0, @"Environments.plist file not found");
-    
+
     _environments = [NSDictionary dictionaryWithContentsOfFile:envsFile];
     _targetBuildConfigurationName = [[[NSBundle mainBundle] infoDictionary] objectForKey:kJIMEnvironmentsTargetBuildConfigurationName];
     _environment = _environments[_targetBuildConfigurationName];
-    
+
     return self;
 }
 
